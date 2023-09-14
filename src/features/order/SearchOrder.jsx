74736788085b3af84d32b2389787dc7abeb1f2c9
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-09-13 23:36:10 -0500

    Styled form elements using Tailwind
    Pretty cool... Learned a few things about CSS, too!

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -2,27 +2,28 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-	const [query, setQuery] = useState("");
-	const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-	function handleSubmit(event) {
-		event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
 
-		if (!query) return;
+    if (!query) return;
 
-		navigate(`/order/${query}`);
-		setQuery("");
-	}
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				placeholder="Search order #"
-				value={query}
-				onChange={(event) => setQuery(event.target.value)}
-			/>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72 "
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Search order #"
+        value={query}
+      />
+    </form>
+  );
 }
 
 export default SearchOrder;
