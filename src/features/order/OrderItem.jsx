b72476dcf670b6088b2970ac6321ef8c28976abf
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-09-14 23:36:00 -0500

    Styled the Order Overview
    Finally finished the Tailwind section! This was a really fun section and I definitely feel like I'm going to use Tailwind in future projects.

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,19 +1,19 @@
 import { formatCurrency } from "../../utilities/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
-	const { quantity, name, totalPrice } = item;
-	console.log(quantity, name, totalPrice, isLoadingIngredients, ingredients);
+  const { quantity, name, totalPrice } = item;
+  console.log(quantity, name, totalPrice, isLoadingIngredients, ingredients);
 
-	return (
-		<li>
-			<div>
-				<p>
-					<span>{quantity}&times;</span> {name}
-				</p>
-				<p>{formatCurrency(totalPrice)}</p>
-			</div>
-		</li>
-	);
+  return (
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
+        <p>
+          <span className="font-bold">{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
 }
 
 export default OrderItem;
