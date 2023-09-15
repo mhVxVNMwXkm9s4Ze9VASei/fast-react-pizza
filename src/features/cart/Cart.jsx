46c79b59912339a61a1c1cbb21f54540208c50f5
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-09-14 22:23:32 -0500

    Styled the Cart and related components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -30,7 +31,7 @@ function Cart() {
   console.log(cart);
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <Link
         className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
         to="/menu"
@@ -38,12 +39,18 @@ function Cart() {
         &larr; Back to menu
       </Link>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {fakeCart.map((item) => (
+          <CartItem key={item.key} item={item} />
+        ))}
+      </ul>
 
-      <div>
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
+        <Button type="secondary">Clear cart</Button>
         <button>Clear cart</button>
       </div>
     </div>
--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,19 +1,21 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utilities/helpers";
 
 function CartItem({ item }) {
-	const { pizzaId, name, quantity, totalPrice } = item;
-	console.log(pizzaId, name, quantity, totalPrice);
+  const { pizzaId, name, quantity, totalPrice } = item;
+  console.log(pizzaId, name, quantity, totalPrice);
 
-	return (
-		<li>
-			<p>
-				{quantity}&times; {name}
-			</p>
-			<div>
-				<p>{formatCurrency(totalPrice)}</p>
-			</div>
-		</li>
-	);
+  return (
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
+        {quantity}&times; {name}
+      </p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
+      </div>
+    </li>
+  );
 }
 
 export default CartItem;
