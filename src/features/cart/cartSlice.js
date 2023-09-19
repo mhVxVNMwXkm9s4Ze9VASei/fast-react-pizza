c4822cfbacb807321b7e23484e45ad76d101f182
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-09-18 23:13:58 -0500

    Created cartSlice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      name: "Mediterranean",
+      pizzaId: 12,
+      quantity: 2,
+      totalPrice: 32,
+      unitPrice: 16,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+
+    clearCart(state) {
+      state.cart = [];
+    },
+
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+  },
+});
+
+export const {
+  addItem,
+  clearCart,
+  deleteItem,
+  decreaseItemQuantity,
+  increaseItemQuantity,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
