d68a69b2875bd12f9c51294269cc469f39a54ec6
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-09-20 00:06:53 -0500

    Added ability to remove items from the cart.
    @_@
    
    At least I finally got rid of some errors/bugs!

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -54,6 +54,9 @@ export default cartSlice.reducer;
 
 export const getCart = (state) => state.cart.cart;
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
